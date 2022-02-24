4e7648d63ee281f5957fa1719c15fe38f0d87922
DinoPranjic <dinopranjic92@gmail.com>
2022-02-24 02:22:55 +0000

    implemented printpasses function to test nextISSTimesForMyLocation function

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-/* TEST CODE
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPasses = function(passes) {
+  for (const pass of passes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((err, passes) => {
+  if (err) {
+    return console.log('It didn\'t work!', err);
+  }
+
+  printPasses(passes);
+
+});
+
+/* ORIGINAL TEST CODE
 const { fetchMyIp, fetchCoordsByIp } = require('./iss');
 fetchMyIp((err, ip) => {
   if (err) {
