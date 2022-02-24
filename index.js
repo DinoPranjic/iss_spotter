e2c2db764ac7fa54a930d1da380672141de0b5cf
DinoPranjic <dinopranjic92@gmail.com>
2022-02-24 01:54:21 +0000

    created test code to test fetchISSFlyOverTimes function

--- index.js
+++ index.js
@@ -18,4 +18,16 @@ fetchCoordsByIp('24.57.142.32', (err, coordinates) => {
 
   console.log('It worked! Returned coordinates:', coordinates);
 })
+
+const { fetchMyIp, fetchCoordsByIp, fetchISSFlyOverTimes } = require('./iss');
+const testCoordinates = { latitude: 42.265, longitude: -83.0283 };
+
+fetchISSFlyOverTimes(testCoordinates, (err, passes) => {
+  if (err) {
+    console.log('It didn\'t work!', err);
+    return;
+  }
+
+  console.log('It worked! Returned passes of ISS:', passes);
+})
 */
