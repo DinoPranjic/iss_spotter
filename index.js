dc489a7c5f3a5855f97d16b93c00cab368cc736c
DinoPranjic <dinopranjic92@gmail.com>
2022-02-24 01:39:28 +0000

    added temporary test code to test fetchCoordsByIp function

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const { fetchMyIp } = require('./iss');
-
 /* TEST CODE
+const { fetchMyIp, fetchCoordsByIp } = require('./iss');
 fetchMyIp((err, ip) => {
   if (err) {
     console.log('It didn\'t work!', err);
@@ -9,4 +8,14 @@ fetchMyIp((err, ip) => {
 
   console.log('It worked! IP returned:', ip);
 });
+
+
+fetchCoordsByIp('24.57.142.32', (err, coordinates) => {
+  if (err) {
+    console.log('It didn\'t work!', err)
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:', coordinates);
+})
 */
